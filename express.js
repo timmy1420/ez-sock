0125d22800085c1a8c8bb98d187d1bd9f6b46f36
timmy1420 <tim.poco01@gmail.com>
2021-02-23 15:00:10 -0300

    API endpoint working

--- express.js
+++ express.js
@@ -1,30 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const httpServer = require('http').Server(app);
 const port = 3000;
 const io = require('socket.io')(httpServer);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-app.get('/users', (req, res) => {
+// App setting
+const logging = true;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+//   app.use(express.static('./'))
+// });
+app.post('/event_api', (req, res) => {
+    const { event, message } = req.body;
+    if ( logging ) console.log('API Body: ', req.body);
+
     res.send('Random event!')
-    io.emit('hello', "Users");
+    io.emit(event, message);
 });
 
 io.on('connection', socket => {
-    console.log('connect');
+    if ( logging ) console.log('connect');
 
-    let counter = 0;
-    setInterval(() => {
-      socket.emit('server_data', ++counter);
-    }, 1000);
+    // let counter = 0;
+    // setInterval(() => {
+    //   socket.emit('server_data', ++counter);
+    // }, 1000);
 
     socket.on('client_data', data => {
-        console.log('CLIENT DATA: ', data);
+        if ( logging ) console.log('CLIENT DATA: ', data);
     });
 });
 
 httpServer.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://ip_addr:${port}`)
 })
