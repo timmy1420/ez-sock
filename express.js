5781681057e386cbb7da638d85c038986d77acf0
timmy1420 <tim.poco01@gmail.com>
2021-03-15 12:46:46 -0300

    Socket channel joining working

--- express.js
+++ express.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const httpServer = require('http').Server(app);
 const port = 3000;
@@ -8,34 +7,35 @@ const io = require('socket.io')(httpServer);
 // App setting
 const logging = true;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ extended: true }));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-//   app.use(express.static('./'))
-// });
 app.post('/event_api', (req, res) => {
-    const { event, message } = req.body;
+    const { event, message, channel } = req.body;
     if ( logging ) console.log('API Body: ', req.body);
 
-    res.send('Random event!')
-    io.emit(event, message);
+    if ( channel ) {
+        io.sockets.in(channel).emit('server_data', message);
+        res.send('Message sent to channel!');
+    } else {
+        res.send('Message sent without channel!');
+    }
 });
 
-io.on('connection', socket => {
-    if ( logging ) console.log('connect');
-
-    // let counter = 0;
-    // setInterval(() => {
-    //   socket.emit('server_data', ++counter);
-    // }, 1000);
-
-    socket.on('client_data', data => {
-        if ( logging ) console.log('CLIENT DATA: ', data);
+// handle incoming connections from clients
+io.sockets.on('connection', function(socket) {
+    console.log('connect');
+    // once a client has connected, we expect to get a ping from them saying what room they want to join
+    socket.on('channel', function(channel) {
+      socket.join(channel);
+  
+      console.log("Incoming channel: " + channel);
+      io.sockets.in(channel).emit('server_data', `Server ready for channel: ${channel}`);
     });
+
+    // console.log("All sockets:", io.sockets);
 });
 
 httpServer.listen(port, () => {
   console.log(`Example app listening at http://ip_addr:${port}`)
-})
+});
--- index.js
+++ index.js
@@ -9,18 +9,25 @@ const httpServer = require('http').createServer((req, res) => {
 
 const io = require('socket.io')(httpServer);
 
-io.on('connection', socket => {
-    console.log('connect');
+// handle incoming connections from clients
+io.sockets.on('connection', function(socket) {
+  console.log('connect');
+  // once a client has connected, we expect to get a ping from them saying what room they want to join
+  socket.on('channel', function(channel) {
+    socket.join(channel);
 
-    let counter = 0;
-    setInterval(() => {
-      socket.emit('server_data', ++counter);
-    }, 1000);
-
-    socket.on('client_data', data => {
-        console.log('CLIENT DATA: ', data);
-    });
+    console.log("Incoming channel: " + channel);
+  });
 });
+
+// now, it's easy to send a message to just the clients in a given room
+var channel = "abc123",
+    counter = 0;
+
+setInterval(() => {
+  console.log("Sending data to: " + channel);
+  io.sockets.in(channel).emit('server_data', ++counter);
+}, 1000);
 
 httpServer.listen(3000, () => {
   console.log('go to http://localhost:3000');
