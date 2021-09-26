764e84c9527223554be4a020bee66f69e8a5e9ab
timmy1420 <tim.poco01@gmail.com>
2021-09-26 16:54:02 -0300

    Add NO_CHANNEL for now

--- express.js
+++ express.js
@@ -126,7 +126,7 @@ io.sockets.on('connection', function(socket) {
 
       // Broadcast to other clients
       var disconnected_client = socket_ids.find( client => client.socket_id === socket_id);
-      io.sockets.in(disconnected_client.channel).emit('on_disconnect', `${disconnected_client.socket_id} disconnected from ${disconnected_client.channel} (${socket_ids.length})`);
+      io.sockets.in(disconnected_client.channel).emit('on_disconnect', `${disconnected_client.socket_id} disconnected from ${disconnected_client.channel ? disconnected_client.channel : 'NO CHANNEL'} (${socket_ids.length})`);
 
       // Remove disconnected socket id from clients
       socket_ids = socket_ids.filter( client => client.socket_id !== socket_id );
