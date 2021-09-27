a3ba9eb1b44f379104813294787a158fbc69b550
timmy1420 <tim.poco01@gmail.com>
2021-09-26 21:34:40 -0300

    ...

--- express.js
+++ express.js
@@ -83,9 +83,8 @@ let reserved_events = [
 
 // handle incoming connections from clients
 io.sockets.on('connection', function(socket) {
-    console.log('connect');
-
-    var socket_id = socket.client.conn.id;
+  var socket_id = socket.client.conn.id;
+  console.log('connect from :', socket_id);
 
     socket.onAny((eventName, channel, data) => {
       // Match current event with preserved events
